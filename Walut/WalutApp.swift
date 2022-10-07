cb790944563ac01862104783cf006e22bb40401e
Marcin Bartmiński <mbdjj@o2.pl>
2022-10-07 15:04:30 +0200

    Initial Commit

---
+++ Walut/WalutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalutApp.swift
+//  Walut
+//
+//  Created by Marcin Bartminski on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WalutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
