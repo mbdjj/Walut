df28d28301a58d0b05c7cdfda7687464604a57b2
Marcin Bartmiński <mbdjj@o2.pl>
2023-03-02 10:13:46 +0100

    :sparkles: Add feature request button

--- Walut/Views/FeatureRoadmapView.swift
+++ Walut/Views/FeatureRoadmapView.swift
@@ -32,7 +32,35 @@ struct FeatureRoadmapView: View {
     
     var body: some View {
         RoadmapView(configuration: configuration)
-            .navigationTitle("settings_roadmap")
+            .navigationTitle("features")
+            .toolbar {
+                Button {
+                    sendEmail()
+                } label: {
+                    Image(systemName: "questionmark.square.fill")
+                        .overlay(alignment: .topTrailing) {
+                            Image(systemName: "plus.circle.fill")
+                                .symbolRenderingMode(.multicolor)
+                                .imageScale(.small)
+                                .background(.background)
+                                .clipShape(Circle())
+                                .offset(x: 6, y: -6)
+                        }
+                }
+            }
+    }
+    
+    func sendEmail() {
+        let subject = "Walut - Feature request"
+        let subjectEncoded = subject.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+        
+        let url = URL(string: "mailto:marcin@bartminski.ga?subject=\(subjectEncoded)")
+        
+        if let url, UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        } else {
+            print("D:")
+        }
     }
 }
 
