cbb4148b8fafb36586836ef908cbf5183a5d5967
Marcin Bartmiński <mbdjj@o2.pl>
2023-12-08 10:13:13 +0100

    :fire: Remove duplicate deletion as it caused app crashes

--- Walut/Views/CurrencyListView.swift
+++ Walut/Views/CurrencyListView.swift
@@ -210,19 +210,6 @@ struct CurrencyListView: View {
                 $0.nextRefresh < minInterval || $0.nextRefresh > nextUpdate - (12 * 3600) && $0.nextRefresh < nextUpdate
             })
             
-            let crossReference = Dictionary(grouping: savedCurrencies, by: \.idString)
-            let duplicates = crossReference
-                .filter { $1.count > 1 }
-                .flatMap { $0.1 }
-            
-            try duplicates.forEach { duplicate in
-                let dupeString = duplicate.idString
-                try modelContext.delete(model: SavedCurrency.self, where: #Predicate {
-                    $0.idString == dupeString
-                })
-                modelContext.insert(duplicate)
-            }
-            
             print("Old data successfully deleted")
         } catch {
             print("Couldn't delete old data")
